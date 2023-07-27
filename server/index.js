39172545d4357c827d644654db62df4831432ff5
Kpraveen07 <Kpraveenk701@gmail.com>
2023-07-27 15:33:03 +0530

    modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/MERN")
+mongoose.connect('mongodb+srv://Praveenaws:<RK3xDvMANtRBAp57>@mern.ximp82z.mongodb.net/mern?retryWrites=true&w=majority')
 
 app.get("/", (req, res) => {
     ItemsModel.find({})
